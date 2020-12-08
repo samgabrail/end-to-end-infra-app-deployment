866bf9df679c9698301ec858fa63a668a302e576
samgabrail <sameh.gabrail@gmail.com>
2020-12-08 16:16:07 -0500

    fixed a paranthesis

--- Terraform/ProvisionAppVMs/main.tf
+++ Terraform/ProvisionAppVMs/main.tf
@@ -172,7 +172,7 @@ resource "azurerm_linux_virtual_machine" "webblog" {
 
   tags = local.common_tags
 
-  network_interface_ids = [element(azurerm_network_interface.webblog-nic.*.id, var.vm_names[each.value]]
+  network_interface_ids = [element(azurerm_network_interface.webblog-nic.*.id, var.vm_names[each.value])]
   // Add a public key to the same folder as the main.tf script (we use Ansible to send the private key to the Jenkins machine)
   admin_ssh_key {
     username   = var.adminuser
